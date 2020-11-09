82c83602a5f05b592dc5b6d29e7247d9a79175a7
David <416dfranco@gmail.com>
2020-11-09 15:41:35 -0500

    made otherTest

---
+++ src/test/java/OtherTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OtherTest {
+
+    @Test
+    public void test(){
+        assertEquals(5,5);
+    }
+}
